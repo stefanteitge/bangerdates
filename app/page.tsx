444aef8c127a86126dc5f84b910223803286b24e
Stefan Teitge <stefan.teitge@gmail.com>
2024-11-19 18:21:45 +0100

    A litlle bit of typography

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,23 @@ export function MyDate({meeting} : MyDateProps) {
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          {getDates().map(x => <MyDate meeting={x} />)}
+    <div className="grid grid-rows-[20px_1fr_20px] items-top justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            <h1 className="text-5xl font-extrabold">Banger dates </h1>
+            <div className="text-2xl italic text-neutral-500">Belgium & Netherlands</div>
+            <h2 className="text-3xl font-extrabold">Next races</h2>
+            {getDates().map(x => <MyDate meeting={x}/>)}
 
-          <table>
-            <tbody>
-              {listRaceSundays().map(s => <tr key={s.toLocaleString()}><td>{s.toLocaleString()}</td></tr>)}
-            </tbody>
-          </table>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+            <h2 className="text-3xl font-extrabold">Race by weekend (not-working)</h2>
+            <table className="table-auto">
+                <tbody>
+                {listRaceSundays().map(s => <tr key={s.toLocaleString()}>
+                    <td>{s.toLocaleString()}</td>
+                </tr>)}
+                </tbody>
+            </table>
+        </main>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         Just banger dates. And maybe some stock car dates too.
       </footer>
     </div>
