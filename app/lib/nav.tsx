f4fcf72d1470e06254dec5b19849cc4c796416e6
Stefan Teitge <stefan.teitge@gmail.com>
2024-12-11 13:29:35 +0000

    Add icons

--- app/lib/nav.tsx
+++ app/lib/nav.tsx
@@ -1,17 +1,20 @@
 import Link from 'next/link';
+import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 
 export default function Nav() {
   return (
     <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center">
       <div className="fixed bottom-4 mx-auto rounded bg-slate-300 p-2 shadow-md">
         <Link className="p-2" href="/">
-          Index
+          <HomeOutlinedIcon fontSize="large" />
         </Link>
         <Link className="p-2" href="/archive">
-          Archive
+          <ArchiveOutlinedIcon fontSize="large" />
         </Link>
         <Link className="p-2" href="/about">
-          About
+          <InfoOutlinedIcon fontSize="large" />
         </Link>
       </div>
     </div>
