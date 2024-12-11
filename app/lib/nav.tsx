3a6e02485575c9efa2ebff8a103255e68570d73d
Stefan Teitge <stefan.teitge@gmail.com>
2024-12-11 12:57:20 +0000

    Nav improved

--- app/lib/nav.tsx
+++ app/lib/nav.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Nav() {
   return (
     <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center">
-      <div className="fixed bottom-2 mx-auto rounded bg-slate-300 p-4">
+      <div className="fixed bottom-4 mx-auto rounded bg-slate-300 p-2 shadow-md">
         <Link className="p-2" href="/">
           Index
         </Link>
