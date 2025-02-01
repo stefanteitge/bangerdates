acad3bc225488987e2af8960be1044c78bd757c5
Stefan Teitge <stefan.teitge@gmail.com>
2025-02-01 19:33:19 +0000

    Add ical

---
+++ app/ical/route.ts
@@ -0,0 +1,26 @@
+import { listUpcomingRaceDays2 } from '@/app/lib/race-repository';
+import ical, { ICalCalendarMethod } from 'ical-generator';
+export const dynamic = 'force-static';
+
+export async function GET() {
+  const calendar = ical({ name: 'Banger dates' });
+
+  const raceDays = listUpcomingRaceDays2();
+
+  raceDays
+    .filter((r) => r.race)
+    .forEach((r) => {
+      calendar.createEvent({
+        start: r.date.toLocaleString(),
+        summary: 'Race in ' + r.race?.location,
+        location: r.race?.location,
+      });
+    });
+
+  // A method is required for outlook to display event as an invitation
+  calendar.method(ICalCalendarMethod.PUBLISH);
+
+  return new Response(calendar.toString(), {
+    status: 200,
+  });
+}
