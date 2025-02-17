cec7090f6b2a2a9b9082250b27e4fecffcb5acd7
Stefan Teitge <stefan.teitge@gmail.com>
2025-02-17 07:29:58 +0000

    Add missing type

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,5 +1,11 @@
 import { Temporal } from 'temporal-polyfill';
 
+export interface RaceMeeting {
+  date: Temporal.PlainDate;
+  location: string;
+  classes: string[];
+}
+
 export interface RaceMeeting2 {
   date: Temporal.PlainDate;
   location: string;
